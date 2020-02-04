6b984a65d3d8ec0b9b7a374b42734d04ba3e9081
Wolfy <ricardo.villalobos1@marist.edu>
2020-02-03 20:14:26 -0500

    loop project

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,6 @@
-#constructing a sentence
-noun="Bed"
-verb="sleep"
-adjective="Big"
-place="the marist dorms"
+#constructing a sentencen
+noun=input("enter a noun")
+verb=input("enter a verb")
+adjective=input("enter a adj")
+place=input("enter a place")
+print("take your",adjective,noun,"and",verb,"it at the",place)

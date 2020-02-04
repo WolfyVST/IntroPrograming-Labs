72ae60571fe42adb650f824c01367fe477706173
Wolfy <ricardo.villalobos1@marist.edu>
2020-02-03 20:07:05 -0500

    loop project

--- game.py
+++ game.py
@@ -7,16 +7,21 @@ cartadel1=random.randrange(1,11)
 cartadel2=random.randrange(1,11)
 cartfinaldel=cartadel1+cartadel2
 cartafinaljug=cartjug1+cartjug2
-respuesta=input("Do you want to play baclk jack ")
 # el mero juego
+respuesta=input("Do you want to play baclk jack ")
 if respuesta=="yes"or respuesta=="Yes":
  print("sus cartas son",cartjug1,cartjug2)
  print("las cartas del dealer son:",cartadel1,"??")
- if cartfinaldel>21:
+ respuesta2 = input("Would you like another card?")
+ if respuesta2 =="yes" or respuesta2=="Yes":
+     cartafinaljug=cartafinaljug+random.randrange(1,11)
+     print("la suma de sus cartas es",cartafinaljug)
 
+ if cartfinaldel>21:
+     print("you have won dealer got busted")
  if cartfinaldel==21:
-     print"You lost the deler has won the game he got a Black jack"
- else:
+     print("the dealer has won he got a black jack")
+
 
 else:
     print("thanks for playing")

9da4f907d659e87444a11e85f7a9ee42813f2e20
Wolfy <ricardo.villalobos1@marist.edu>
2020-02-10 16:42:00 -0500

    Drying my code out

--- game.py
+++ game.py
@@ -7,39 +7,45 @@ cartadel1=random.randrange(1,11)
 cartadel2=random.randrange(1,11)
 cartfinaldel=cartadel1+cartadel2
 cartafinaljug=cartjug1+cartjug2
-
-#cards = []
-#cards.append(4)
-#sum(cards)
+score= 0
 
 # el mero juego
-respuesta=input("Do you want to play baclk jack ").lower()
+print("You enter a tavern after traveling all day long in the sunny dry desert you orlder a drink and sit at the "
+      "cornered table")
+nombre= input("Someone sits next to you and says: hello fellow traver whats your name?" )
+respuesta=input("care fora game of black jack?").lower()
 while respuesta=="yes":
- print("sus cartas son",cartjug1,cartjug2)
- print("las cartas del dealer son:",cartadel1,"??")
- respuesta2 = input("Would you like another card?").lower()
+ print("your cards are: ",cartjug1,cartjug2)
+ print("Dealers Cards are: ",cartadel1,"??")
+ respuesta2 = input("Would you like another card? ").lower()
  while respuesta2 == "yes":
      cartafinaljug=cartafinaljug+random.randrange(1,11)
-     print("la suma de sus cartas es",cartafinaljug)
+     print("the sum of your cards is: ",cartafinaljug)
      respuesta2=input("Would you like another card")
  if cartafinaljug > 21:
      print("The dealer has won you got busted")
-     respuesta="no"
+     score= score - 5
+     respuesta = input("do you want to play again")
  if cartfinaldel < 16:
-   cartfinaldel = cartfinaldel + random.randrange(1 , 11)
+    cartfinaldel = cartfinaldel + random.randrange(1 , 11)
  if cartfinaldel > 21:
      print ("you have won dealer got busted")
-     respuesta="no"
+     score = score + 10
+     respuesta = input("do you want to play again")
  if cartfinaldel == 21:
      print("the dealer has won he got a black jack")
-     respuesta="no"
+     score = score - 5
+     respuesta = input("do you want to play again")
  if cartfinaldel > cartafinaljug:
      print("The dealer has won hes got: ",cartfinaldel,"and you have: ",cartafinaljug)
-     respuesta="no"
+     score = score-5
+     respuesta = input("do you want to play again")
  if cartafinaljug > cartfinaldel:
      print("You have won you got: ",cartafinaljug,"and the dealer has: ",cartfinaldel)
- respuesta= input("do you want to play again")
+     score=score + 10
+     respuesta= input("do you want to play again").lower()
 
 
 else:
- print("thanks for playing")
+ print("Thanks for playing with me",nombre)
+ print("your score is:",score)

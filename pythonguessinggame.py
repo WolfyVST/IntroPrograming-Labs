70f5396b0ca8e80c5eb2372672b97516719f1146
Wolfy <ricardo.villalobos1@marist.edu>
2020-02-17 17:24:00 -0500

    Initial version of a simple guessing game.

---
+++ pythonguessinggame.py
@@ -0,0 +1,11 @@
+guess = "lion"
+print("Hello ready to play a guessing game?")
+print("Try guessing the animal, the animal is know as the king of the jungle!")
+awnser1 = input("ready?").lower()
+while awnser1 == "yes" :
+    awnser2 = input("whats your guess?")
+    if awnser2 == guess :
+        print("well done, your a genius thanks for playing")
+        awnser1 = "no"
+    else:
+        print("Good guess but it is incorrect :( another hint is that the animal has a huge mane")

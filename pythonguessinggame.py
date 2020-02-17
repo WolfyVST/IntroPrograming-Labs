2c7f2e5771eb182bc144d64311249db748e7f8ea
Wolfy <ricardo.villalobos1@marist.edu>
2020-02-17 17:40:58 -0500

    "Usability improvements to the guessing game

--- pythonguessinggame.py
+++ pythonguessinggame.py
@@ -3,8 +3,10 @@ print("Hello ready to play a guessing game?")
 print("Try guessing the animal, the animal is know as the king of the jungle!")
 awnser1 = input("ready?").lower()
 while awnser1 == "yes" :
-    awnser2 = input("whats your guess?")
-    if awnser2 == guess :
+    awnser2 = input("whats your guess?").lower()
+    if awnser2== "quit":
+        awnser1 = "no"
+    elif awnser2 == guess :
         print("well done, your a genius thanks for playing")
         awnser1 = "no"
     else:
